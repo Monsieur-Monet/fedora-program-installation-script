d36f4c5214d437439cc39e39737bc865f196e157
Monsieur-Monet <timoscheer11@gmail.com>
2021-06-06 18:33:33 +0200

    changed echo to printf

--- fpis.sh
+++ fpis.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-echo "Welcome to the fedora program installation script by Monsieur-Monet."
-echo "First, we'll update your system"
+printf "Welcome to the fedora program installation script by Monsieur-Monet.\n"
+printf "First, we'll update your system\n"
 read -p "Would you like to continue? " -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     exit 1
 fi
+printf "\n\n"
 sudo dnf update --refresh
-clear
-echo "Now, we'll install a few flatpaks."
-echo "The following flatpaks will be installed:"
+printf "\n"
+printf "Now, we'll install a few flatpaks.\n"
+printf "The following flatpaks will be installed:\n"
 for flatpakList in Spotify Discord VSCode Mattermost GIMP Signal DéjàDupBackups
 do
     echo $flatpakList
