badc9c921a98b6500ded9266c041d0d318d3fb03
Monsieur-Monet <timoscheer11@gmail.com>
2021-06-08 18:17:02 +0200

    added more stuff. ZSH install is currently broken

--- fpis.sh
+++ fpis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 printf "Welcome to the fedora program installation script by Monsieur-Monet.\n"
 printf "First, we'll update your system\n\n"
-sudo dnf update --refresh
+sudo dnf -y update --refresh
 printf "\n"
 read -p "Do you use a NVIDA GPU with your system?" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
@@ -9,14 +9,14 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     printf "Continuing without installation of NVIDIA drivers\n"
 
 else
-    sudo dnf install fedora-workstation-repositories
+    sudo dnf -y install fedora-workstation-repositories
     sudo dnf config-manager --set-enabled rpmfusion-nonfree-nvidia-driver
-    sudo dnf install nvidia-settings
+    sudo dnf -y install nvidia-settings
 fi
 printf "\n"
 printf "Now, we'll install a few flatpaks.\n"
 printf "The following flatpaks will be installed:\n"
-for flatpakList in Spotify Discord VSCode Mattermost GIMP Signal DéjàDupBackups
+for flatpakList in Spotify Discord VSCode Mattermost GIMP Signal
 do
     echo $flatpakList
 done
@@ -33,19 +33,20 @@ printf "\n"
 printf "Flatpaks have been installed!\n"
 printf "Now we will proceed with some essential software using dnf.\n"
 printf "The following apps will be installed:\n"
-for flatpakList in Steam Terminator Inkscape Wine
+for flatpakList in Steam Terminator Inkscape deja-dup
 do
     echo $flatpakList
 done
 read -p "Would you like to continue?" -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
-fi
+else
 sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y install steam terminator inkscape wine
+sudo dnf -y install steam terminator inkscape deja-dup
+fi
 printf "\n"
 printf "Apps have been installed!\n"
+printf "\n"
 read -p "Would you like to install ZSH on your system?" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     printf "\n\n"
